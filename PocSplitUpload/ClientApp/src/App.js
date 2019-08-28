66d98c1533bbe725149e10a00ad5048e07e93d91
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-08-28 20:09:21 -0300

    Add React components

--- PocSplitUpload/ClientApp/src/App.js
+++ PocSplitUpload/ClientApp/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Upload } from './components/Upload';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -12,8 +11,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route path='/upload' component={Upload} />
       </Layout>
     );
   }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # POC-SplitUpload
-Proof of Concept of a upload split from one C# app to a C# Web API (both .NET Core)
+**Proof of Concept** *(POC)* of a upload splited from **React** app to **C# .NET Core** backend then to a **C# .NET Core** BFI (*Backend For Integration*)
+
+This POC is based on a .NET Core API & React project.
+
+
+UI project:
+```bash
+dotnet new React POCSplitUpload.UI
+```
+
+BFI project:
+```bash
+dotnet new Webapi POCSplitUpload.API
+```
